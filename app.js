b37f8bb4f89f63bf097670e03c6229de702ca2cf
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-09-27 16:54:27 +0300

    Setting Parsers App, Creating Public folder

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4200;
 
-//Parsing mdlw
-//Parsing app
+//Parsing mddlwr
+//Parsing app/urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Parsing Application/json
+app.use(bodyParser.json());
+//Static Files
+app.use(express.static("public"));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
