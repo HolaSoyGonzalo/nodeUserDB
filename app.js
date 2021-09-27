6eab2fd080ed01824c19d9f7d54c879dfa52a1a6
Davide Pacco <72199035+HolaSoyGonzalo@users.noreply.github.com>
2021-09-27 16:43:27 +0300

    Create and update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 4200;
+
+//Parsing mdlw
+//Parsing app
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
